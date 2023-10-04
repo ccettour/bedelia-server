138d0cc9eebc540ef109975ab6edcc1221ebc6a8
Leojg018 <133281276+Leojg018@users.noreply.github.com>
2023-10-04 12:47:33 -0300

    Trate de hacerlo lo mas prolijo asi se entiende que es cada codigo xd
    También tuve que instalar de nuevo los comandos

--- src/baseDeDatos/estudianteBD.js
+++ src/baseDeDatos/estudianteBD.js
@@ -1,22 +1,86 @@
 const conexion = require("./conexionBD");
 
-
 const buscarPorID = async (idEstudiante) => {
+  const consulta =
+    "SELECT dni, nombre, apellido, " +
+    "(CASE " +
+    "  WHEN nacionalidad = 0 THEN 'arg' " +
+    "  WHEN nacionalidad = 1 THEN 'uru' " +
+    "  WHEN nacionalidad = 2 THEN 'chi' " +
+    "  WHEN nacionalidad = 3 THEN 'par' " +
+    "  WHEN nacionalidad = 4 THEN 'bra' " +
+    "  WHEN nacionalidad = 5 THEN 'bol' " +
+    "  ELSE '' " +
+    "END) AS nacionalidad " +
+    "FROM estudiante " +
+    "WHERE activo = 1 AND idEstudiante = ?";
 
-    const consulta = "SELECT * FROM estudiante WHERE activo=1 AND idEstudiante = ?";
+  const [estudiante] = await conexion.query(consulta, idEstudiante);
 
-    const [estudiante] = await conexion.query(consulta, idEstudiante);
+  return estudiante;
+};
 
-    return estudiante;
+
+
+
+///////////BUSCAR TODOS//////////////
+
+
+const buscarTodos = async ()=>{
+
+
+    const consulta = "SELECT dni, nombre, apellido, fechaNacimiento,correoElectronico,celular,foto " +
+    "(CASE " +
+    "  WHEN nacionalidad = 0 THEN 'argentino' " +
+    "  WHEN nacionalidad = 1 THEN 'uruguayo' " +
+    "  WHEN nacionalidad = 2 THEN 'chileno' " +
+    "  WHEN nacionalidad = 3 THEN 'paraguayo' " +
+    "  WHEN nacionalidad = 4 THEN 'brasilero' " +
+    "  WHEN nacionalidad = 5 THEN 'boliviano' " +
+    "  ELSE '' " +
+    "END) AS nacionalidad " +
+    "FROM estudiante " +
+    "WHERE activo = 1";
+
+
+    const [estudiantes] = await conexion.query (consulta);
+
+    return estudiantes;
+    
+
+};
+
+
+
+
+////////////ELIMINAR ESTUDIANTE//////////////
+
+const eliminar = async (idEstudiante)=>{
+    const consulta = 'UPDATE estudiante SET activo = 0 WHERE idEstudiante = ?';
+    await conexion.query(consulta, [idEstudiante]);
 }
 
 
-/*const crear = async () => {}
-const editar = async () => {}
-const buscarTodos = async () => {}
-const eliminar = async () => {}*/
+
+
+
+//////////////////CREAR ESTUDIANTE///////////////////////
+
+
+/*const crear = async (estudiante) =>{
+    const {dni,nombre,apellido,fechaNacimiento,correroElectronico,celular,foto} = estudiante;
+
+    const consutla = 'INSERT INTO estudiante()'
+}*/
+
+
+
 
 
 module.exports = {
-    buscarPorID //, crear, editar, buscarTodos, eliminar
+    buscarPorID,
+    buscarTodos,
+    eliminar,
+    crear
+
 }
--- src/controladores/estudiantes.js
+++ src/controladores/estudiantes.js
@@ -17,6 +17,81 @@ buscarPorId = async(req, res) => {
     }
 }
 
+
+
+
+///////////BUSCAR TODOS //////////////////////
+
+buscarTodos = async (req,res) =>{
+    try {
+        const estudiante = await estudianteBD. buscarTodos();
+
+        res.json ({ estado: 'OK', dato: estudiante});
+    }catch (exec){
+        throw exec;
+
+    }
+    
+}
+
+
+//////////ELIMINAR ESTUDIANTE/////////
+
+
+eliminar = async (req,res) =>{
+    const idEstudiante = req.params.idEstudiante;
+
+
+    if(!idEstudiante){
+        res.status(404).json({estado: 'FALLO', msj: 'no se especifico el id del estudiante'});
+
+    }else {
+        try{
+            await estudianteBD.eliminar(idEstudiante);
+            res.status(200).json({estado: 'OK', msj:'Estudiante eliminado'});
+        }catch(error){
+            console.log(error);
+        }
+    }        
+}
+
+
+///////////////////CREAR ESTUDIANTE////////////////////////////
+
+
+
+
+crear = async (req,res)=>{
+    const {dni, nombre, apellido, fechaNacimiento, nacionalidad,correoElectronico,celular,foto} = req.body;
+
+    if (!din || !nombre || !apellido || !nacionalidad || !correoElectronico){
+        res.status(404).json({estado:'FALLO', msj:'Faltan datos obligatorios'});
+
+    }else{
+        const estudiante = {
+            dni:dni, 
+            nombre:nombre, 
+            apellido:apellido, 
+            fechaNacimiento:fechaNacimiento, 
+            nacionalidad:nacionalidad,
+            correoElectronico:correoElectronico,
+            celular:celular,
+            foto:foto
+        }
+
+        try{
+            const estudianteNuevo = await estudianteBD.nuevo(estudiante);
+            res.status(201).json({estado:'OK', msj:'Estudiante creado', dato:estudianteNuevo});
+        }catch (exec){
+            console.log(exec);
+        }
+    }
+
+}
+
+
 module.exports = {
     buscarPorId,
+    buscarTodos,
+    eliminar 
 }
